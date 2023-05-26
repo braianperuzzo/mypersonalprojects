f80b1f0a0578e514e1eaf2df3da19b65ac9cf822
braianperuzzo <bperuzzo96@hotmail.com>
2023-05-26 16:00:20 -0300

    Primeiro programa que criei em C#, para estudar

---
+++ Aula1Console/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula1Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sCondicao;
+            Console.WriteLine("Olá!");
+            Console.WriteLine("Como você está?");
+            sCondicao = Console.ReadLine();
+
+            Console.WriteLine("A sua condição é: " + sCondicao);
+
+            Console.ReadKey();
+        }
+    }
+}
